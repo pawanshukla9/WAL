b9336c3bb726d74004d5bedb0af9dbc703eeffaa
ashmick12 <ashmi.joseph@ck12.org>
2022-04-02 21:19:39 -0400

    Adding comments.

--- pg_wal.py
+++ pg_wal.py
@@ -7,6 +7,7 @@ from psycopg2.extras import LogicalReplicationConnection
 
 
 try:
+    #Create a new database session and return a new connection object
     my_connection  = psycopg2.connect(
                       "dbname='ADTTest' host='localhost' user='ashmijoseph' password='password'" ,
                       connection_factory = LogicalReplicationConnection)
@@ -20,14 +21,16 @@ except Exception as e:
 
 
 def handler():
+    #Cursors allow to execute PostgreSQL command in a database session
     cur = my_connection.cursor()
     try:
-        cur.create_replication_slot('wal2json_test_slot', output_plugin = 'wal2json')
+        cur.create_replication_slot('wal2json_test_slot', output_plugin = 'wal2json') #Create a logical replication slot
     except:
         pass
-    cur.start_replication(slot_name = 'wal2json_test_slot', options = {'pretty-print' : 1}, decode= True)
+    cur.start_replication(slot_name = 'wal2json_test_slot', options = {'pretty-print' : 1}, decode= True) #Instructs server to start streaming WAL
     
     cur.consume_stream(consume)
+    # ^^^ endless loop or stream: stop with Control-C
 
 overall_rows = []
 columns_names = None
@@ -37,22 +40,23 @@ def consume(msg):
     print (msg.payload)
     print('----------------------------')
     data_incoming = msg.payload
+    #parse the JSON string and convert it into a Dictionary.
     data_incoming = json.loads(data_incoming)
     print(data_incoming)
     print('----------------------------')
     if len(data_incoming.get('change'))> 0:
-        columns_names = data_incoming.get('change')[0].get('columnnames')
-        columns_values = data_incoming.get('change')[0].get('columnvalues')
-        table_name = data_incoming.get('change')[0].get('table')
-        kind = data_incoming.get('change')[0].get('kind')
+        columns_names = data_incoming.get('change')[0].get('columnnames') #get columnNames
+        columns_values = data_incoming.get('change')[0].get('columnvalues') #get columnValues
+        table_name = data_incoming.get('change')[0].get('table') #get DB tableName
+        kind = data_incoming.get('change')[0].get('kind') #get DB operation performed
         columns_names.append('kind')
         columns_values.append(kind)
-        FILE_LOCATION = FILE_LOCATION + table_name + '.csv'
+        FILE_LOCATION = FILE_LOCATION + table_name + '.csv' #create a separate csv file for each table
         try :
-            df = pd.read_csv(FILE_LOCATION, index_col = False)
+            df = pd.read_csv(FILE_LOCATION, index_col = False) #Read a csv file into DataFrame
             df_delta = pd.DataFrame([columns_values], columns= columns_names)
             df = df.append(df_delta,ignore_index = True)
-            df.to_csv(FILE_LOCATION, index=False)
+            df.to_csv(FILE_LOCATION, index=False) #Write object to a csv file
         except Exception as e: 
             print(str(e))
             print('EXCEPTION')
